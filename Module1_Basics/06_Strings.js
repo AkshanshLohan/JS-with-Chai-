a6a5ce440eb4a690ee526ba20f448bd2a21e4ebf
AkshanshLohan <akshanshlohan@gmail.com>
2024-09-20 01:12:08 +0530

    strings

---
+++ Module1_Basics/06_Strings.js
@@ -0,0 +1,17 @@
+const name="Akshansh"
+const repocount=40
+
+console.log(`hello my name is ${name} and my repocount is ${repocount}`)
+//this is called string interpolation
+
+const hobbies="playing cricket"
+console.log(hobbies[0])
+console.log(hobbies.length)
+console.log(hobbies.charAt(5))
+console.log(hobbies.substring(0,5))
+
+const newstring="        xyz       "
+console.log(newstring.trim())//trims the starting and end spaces of a string
+
+const url="https://akshansh.com%20chaudhary"
+console.log(url.replace('%20','-'))
