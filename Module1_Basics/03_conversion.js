6ff446ab66d2d494ed33606bdd479e9cbabb0113
AkshanshLohan <akshanshlohan@gmail.com>
2024-09-20 00:00:56 +0530

    conersionofdatatypes

---
+++ Module1_Basics/03_conversion.js
@@ -0,0 +1,15 @@
+let score="30"
+
+let valueInNumber = Number(score)
+//converts string to number
+//first letter capital Number,String
+
+console.log(valueInNumber)
+
+let score2="33abc"
+let value=Number(score2)
+console.log(value)//NaN-not a number
+
+let isloggedin=true
+let boolvalue=Boolean(isloggedin)
+console.log(boolvalue)
