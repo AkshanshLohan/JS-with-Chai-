7d46739784dbec83d82d40ffa667d77ccf7a6618
AkshanshLohan <akshanshlohan@gmail.com>
2024-09-20 00:34:27 +0530

    primitvevsnon

---
+++ Module1_Basics/05_primitiveVSnon.js
@@ -0,0 +1,13 @@
+//primitive vs non primitive data types
+
+//primitive
+ 
+//7 types: boolean,string,number,null,undefined,Symbol,BigInt
+const id= Symbol("23")
+const id2=Symbol("23")
+console.log(id===id2)
+
+
+//Non primitive(reference types)
+
+//Arrays,Objects,Functions
