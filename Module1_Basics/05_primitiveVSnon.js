ad374da78e41be7eba2316d4c78c90333db8bb56
AkshanshLohan <akshanshlohan@gmail.com>
2024-09-20 00:36:59 +0530

    update

--- Module1_Basics/05_primitiveVSnon.js
+++ Module1_Basics/05_primitiveVSnon.js
@@ -1,6 +1,6 @@
 //primitive vs non primitive data types
 
-//primitive
+//primitive- stack memory is used for primitive
  
 //7 types: boolean,string,number,null,undefined,Symbol,BigInt
 const id= Symbol("23")
@@ -8,6 +8,6 @@ const id2=Symbol("23")
 console.log(id===id2)
 
 
-//Non primitive(reference types)
+//Non primitive(reference types)- heap memory is used for non primitive
 
 //Arrays,Objects,Functions
