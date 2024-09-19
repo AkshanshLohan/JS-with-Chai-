b82444ef3a4a4fd36361c480a9ae8a83c25d3c96
AkshanshLohan <akshanshlohan@gmail.com>
2024-09-20 00:15:53 +0530

    confusion

--- Module1_Basics/04_confusion.js
+++ Module1_Basics/04_confusion.js
@@ -1,2 +1,11 @@
-console.log("1"+2+2)
-console.log(1+2+"2")
+console.log("1"+2+2)//122
+
+// "1"+2 => string me 2 add hua "12" string result mila
+
+
+
+
+
+console.log(1+2+"2")//32
+
+// 1+2=3 then 3+"2"=32 string me 3 add ho gya
