acffdaae145ac0c411aaa453b35a97e9bad032ac
AkshanshLohan <akshanshlohan@gmail.com>
2024-09-21 00:28:29 +0530

    obj

---
+++ Module3_objects/01_objects.js
@@ -0,0 +1,18 @@
+//singleton  by constructor
+//object.create
+
+//object literals
+const mysym=Symbol("key1")
+
+const jsuser={name:"akshu",
+    "full name" :"Akshansh Chaudhary",
+     mob:"9090909090", 
+     [mysym]:"mykey1",//we have to use square bracket for symbol
+     age:"18",
+     logindays:["mon","sat"]
+    }
+
+    console.log(jsuser.age)
+  //  console.log(jsuser.full name) //error cannot access it using dot
+    console.log(jsuser["full name"])
+    console.log(jsuser[mysym])
