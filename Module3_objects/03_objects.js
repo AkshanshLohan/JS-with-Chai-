22adac49cebe63cd25e73b3df8b07ac318fba6b0
AkshanshLohan <akshanshlohan@gmail.com>
2024-09-22 14:40:50 +0530

    03objects

---
+++ Module3_objects/03_objects.js
@@ -0,0 +1,9 @@
+//de structuring
+const course={
+    coursename:"dsa in java",
+    price:"1999",
+    courseinstructor: "akshansh"
+}
+
+const {courseinstructor: instructor}=course
+console.log(courseinstructor)
