ad8c815e707b307629c9994785dcdf62feb065ec
AkshanshLohan <148639938+AkshanshLohan@users.noreply.github.com>
2024-12-25 21:09:57 +0530

    Update 08_dates.js

--- Module1_Basics/08_dates.js
+++ Module1_Basics/08_dates.js
@@ -1,5 +1,5 @@
 let myDate=new Date()
-console.log(myDate.toDateString())
-console.log(myDate.toISOString())
-console.log(myDate.toJSON())
-console.log(myDate.toLocaleDateString())
+console.log(myDate.toDateString())//Fri Oct 06 2023
+console.log(myDate.toISOString()) //2023-10-06T12:34:56.789Z
+console.log(myDate.toJSON()) //2023-10-06T12:34:56.789Z
+console.log(myDate.toLocaleDateString()) //10/6/2023
