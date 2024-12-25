b3449b7596d8128ad52375a91e6dca6d64dfa729
AkshanshLohan <148639938+AkshanshLohan@users.noreply.github.com>
2024-12-25 21:39:07 +0530

    Update 03_objects.js

--- Module3_objects/03_objects.js
+++ Module3_objects/03_objects.js
@@ -1,9 +1,9 @@
-//de structuring
+//de structuring- It is just syntactical sugar
 const course={
     coursename:"dsa in java",
     price:"1999",
     courseinstructor: "akshansh"
 }
 
-const {courseinstructor: instructor}=course
-console.log(courseinstructor)
+const {courseinstructor: instructor}=course// curly brackets ka syntax dekhke smjh jao ki yaha par de structuring ki ja rhi h
+console.log(courseinstructor)//akshansh
